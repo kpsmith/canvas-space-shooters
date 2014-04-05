f03ed3e59962b184ec0c259bf894889fca869daf
Keith Smith <keith.smith@webfilings.com>
2014-04-05 16:45:24 -0600

    basic game logic with explosions

--- swimmers/logic.js
+++ swimmers/logic.js
@@ -8,34 +8,60 @@ window.requestAnimFrame = (function(callback) {
 
 $(document).ready(function() {
     var canvas = document.getElementById('drawArea');
-    canvas.height = $(window).height();
-    canvas.width = $(window).width();
-    $("#drawArea").css("width", $(window).width());
-    $("#drawArea").css("height", $(window).height());
     var context = canvas.getContext('2d');
-    var players = [];
+    var scene = [];
 
-    function init() {
-        players.push(player(0, canvas.height / 2, 20, 'blue', 7, 0));
-        players.push(player(0, canvas.height / 3, 20, 'yellow', 2, 1));
-        players.push(player(0, canvas.height / 4, 20, 'red', 3, 1));
-        players.push(player(0, canvas.height / 5, 20, 'green', 4, 1));
-        players.push(player(0, canvas.height / 6, 20, 'purple', 5, 1));
-        players.push(player(0, canvas.height / 7, 20, 'orange', 6, 1));
+    function resizeCanvas() {
+        var widthWanted = $(window).width();
+        var heightWanted = $(window).height();
+
+        canvas.width = widthWanted;
+        canvas.height = heightWanted;
+
+        $("#drawArea").css("width", widthWanted);
+        $("#drawArea").css("height", heightWanted);
+    }
+    $(window).resize(resizeCanvas());
+
+    // Init
+    (function() {
+        resizeCanvas();
+        scene.push(player(canvas.width * Math.random(), canvas.height * Math.random(), 20, 'blue', 2.0, 0));
+        scene.push(player(canvas.width * Math.random(), canvas.height * Math.random(), 20, 'purple', 2.0, 1));
+        scene.push(player(canvas.width * Math.random(), canvas.height * Math.random(), 20, 'yellow', 2.0, 1));
+        // scene.push(particle(100,100,2,3,0.5,20,'red'));
         animate();
         doLogic();
-    }
-    init();
 
-    function player(x, y, r, color, vel, id) {
+        function foo() {
+            scene[0].fire(scene[1]);
+            setTimeout(foo, randFloat(500, 1000));
+        }
+        foo();
+
+        function bar() {
+            scene[1].fire(scene[2]);
+            setTimeout(bar, randFloat(500, 1000));
+        }
+        bar();
+
+        function baz() {
+            scene[2].fire(scene[0]);
+            setTimeout(baz, randFloat(500, 1000));
+        }
+        baz();
+
+    })();
+
+    function player(x, y, r, color, vel, idx) {
         return {
-            id: id,
+            idx: idx,
             x: x,
             y: y,
             r: r,
             color: color,
             dir: 1,
-            yDir:1,
+            yDir: 1,
             vel: vel,
             draw: function() {
                 context.beginPath();
@@ -47,44 +73,134 @@ $(document).ready(function() {
                 context.stroke();
             },
             think: function() {
-                if (this.id == 0) {
-                    this.x += this.dir * this.vel;
-                    if (this.x > canvas.width) {
-                        this.dir = -1;
-                    }
-                    if (this.x < 0) {
-                        this.dir = 1;
-                    }
-                    this.y += this.yDir * this.vel;
-                    if (this.y > canvas.height) {
-                        this.yDir = -1;
-                    }
-                    if (this.y < 0) {
-                        this.yDir = 1;
-                    }
-                } else {
-                    var targetPlayer = players[0];
-                    var deltaX = targetPlayer.x - this.x;
-                    var deltaY = targetPlayer.y - this.y;
+                this.x += this.dir * this.vel;
+                if (this.x > canvas.width) {
+                    this.dir = -1;
+                }
+                if (this.x < 0) {
+                    this.dir = 1;
+                }
+                this.y += this.yDir * this.vel;
+                if (this.y > canvas.height) {
+                    this.yDir = -1;
+                }
+                if (this.y < 0) {
+                    this.yDir = 1;
+                }
+            },
+            fire: function(targetPlayer) {
+                scene.push(projectile(this.x, this.y, targetPlayer));
+            }
+        };
+    }
 
-                    var factor = this.vel / Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2) );
-                    this.x += factor * deltaX;
-                    this.y += factor * deltaY;
+    function projectile(x, y, targetPlayer) {
+        return {
+            x: x,
+            y: y,
+            r: 4,
+            color: 'red',
+            dir: 1,
+            yDir: 1,
+            vel: 4,
+            target: targetPlayer,
+            draw: function() {
+                context.beginPath();
+                context.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+                context.fillStyle = this.color;
+                context.fill();
+                // context.lineWidth = 2;
+                // context.strokeStyle = '#000000';
+                // context.stroke();
+            },
+            think: function() {
+                var deltaX = targetPlayer.x - this.x;
+                var deltaY = targetPlayer.y - this.y;
+                if (Math.abs(deltaX) < targetPlayer.r + this.r && Math.abs(deltaY) < targetPlayer.r + this.r) {
+                    boom(this.x, this.y);
+                    sceneDelete(this);
+                }
+                var factor = this.vel / Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+                this.x += factor * deltaX;
+                this.y += factor * deltaY;
+            }
+        };
+    }
+
+    function particle(x, y, xVel, yVel, scaleSpeed, r, color) {
+        return {
+            x: x,
+            y: y,
+            r: r,
+            color: color,
+            xVel: xVel,
+            yVel: yVel,
+            scaleSpeed: scaleSpeed,
+            draw: function() {
+                context.beginPath();
+                context.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+                context.fillStyle = this.color;
+                context.fill();
+            },
+            think: function() {
+                // move
+                this.x += this.xVel;
+                this.y += this.yVel;
+                // shrink
+                this.r -= this.scaleSpeed;
+                if (this.r < 0) {
+                    sceneDelete(this);
                 }
             }
         };
     }
 
-    function follow() {
+    function boom(x, y) {
+        expl(x, y, "#525252");
+        expl(x, y, "#FFA318");
+    }
 
+    function expl(x, y, color) {
+        var minSize = 5;
+        var maxSize = 15;
+        var count = 10;
+        var minSpeed = 1;
+        var maxSpeed = 5;
+        var minScaleSpeed = 0.4;
+        var maxScaleSpeed = 1;
+        for (var angle = 0; angle < 360; angle += Math.round(360 / count)) {
+            var speed = randFloat(minSpeed, maxSpeed);
 
+            var xVel = speed * Math.cos(angle * Math.PI / 180.0);
+            var yVel = speed * Math.sin(angle * Math.PI / 180.0);
+
+            var scaleSpeed = randFloat(minScaleSpeed, maxScaleSpeed);
+            var radius = randFloat(minSize, maxSize);
+
+            scene.push(particle(x, y, xVel, yVel, scaleSpeed, radius, color));
+
+        }
+    }
+
+    function sceneDelete(obj) {
+        var delIdx;
+        for (var idx in scene) {
+            if (scene[idx] === obj) {
+                delIdx = idx;
+            }
+        }
+        scene.splice(delIdx, 1);
+    }
+
+    function randFloat(min, max) {
+        return min + Math.random() * (max - min);
     }
 
     function doLogic() {
-        for (var idx in players) {
-            players[idx].think();
+        for (var idx in scene) {
+            scene[idx].think();
         }
-        setTimeout(doLogic, 1000 / 200);
+        setTimeout(doLogic, 1000 / 30);
     }
 
     function animate() {
@@ -94,8 +210,8 @@ $(document).ready(function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         // draw stuff
-        for (var idx in players) {
-            players[idx].draw();
+        for (var idx in scene) {
+            scene[idx].draw();
         }
 
         // request new frame
